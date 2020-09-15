25aecff42619d7793d3ab259c1731e57a3d3517a
kirkondrash <kirkondrash@yandex.ru>
2020-09-15 20:00:15 +0300

    alternative load of jpa beans of both boot app and starter

--- src/main/java/com/deutsche/benchmarkstarter/BenchmarkStarterConf.java
+++ src/main/java/com/deutsche/benchmarkstarter/BenchmarkStarterConf.java
@@ -5,11 +5,15 @@ import com.deutsche.benchmarkstarter.dao.MethodMetricRepo;
 import com.deutsche.benchmarkstarter.service.MethodMetricService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @AutoConfigurationPackage
+//@EnableJpaRepositories
+//@EntityScan
 public class BenchmarkStarterConf {
     @Autowired
     MethodMetricRepo methodMetricRepo;
